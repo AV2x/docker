92440761462fd7d9fe3eb7ac798c81ca692df634
AV2x <kronostime@yandex.ru>
2023-05-25 18:32:45 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Docker контейнер PHP + Compose + Mysql + Phpmyadmin + Redis
+
+<h3>Установка</h3>
+Необходимо иметь <a href="https://docs.docker.com/desktop/install/windows-install">docker</a><br>
+Скачать окружение
+```Bash
+git clone https://github.com/AV2x/docker.git
+``` 
+В корне проекта, запустить
+```Bash
+docker-compose up -d
+``` 
+По дефолту будет доступно по: <a href="http://localhost:8888">localhost:8888</a><br>
+Phpmyadmin: <a href="http://localhost:9001">localhost:9001</a><br>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/polyfill-mbstring' => array(
+            'pretty_version' => 'v1.27.0',
+            'version' => '1.27.0.0',
+            'reference' => '8ad114f6b39e2c98a8b0e3bd907732c207c2b534',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+        'symfony/var-dumper' => array(
+            'pretty_version' => 'v6.2.10',
+            'version' => '6.2.10.0',
+            'reference' => '41a750a23412ca76fdbbf5096943b4134272c1ab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/var-dumper',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
